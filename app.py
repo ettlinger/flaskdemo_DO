046d0a0a91115c15e715c1f277dbc0d76f3f6c05
Marc Ettlinger <ettlinger@gmail.com>
2016-03-19 19:36:41 -0400

    adding bokeh

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ def index():
 def graph():
     symbol = request.form['ticker']
     print("Symbol %s" % symbol)
-    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % symbol
-    session = requests.Session()
-    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-    raw_data = session.get(api_url)
-    TOOLS = 'box_zoom,box_select,resize,reset,hover,wheel_zoom'
-    plot = figure(tools=TOOLS, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime')
+#    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % symbol
+#    session = requests.Session()
+#    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+#    raw_data = session.get(api_url)
+    plot = figure(tools='box_zoom,box_select,resize,reset,hover,wheel_zoom', title='Closing Stock Price', x_axis_label='date', x_axis_type='datetime')
+    plot.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=2)
     script, div = components(plot)
     return render_template('graph.html', script=script, div=div, ticker=symbol)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-#    app.run(host='0.0.0.0', debug=True)
+#    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
