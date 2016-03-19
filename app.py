7b277e1cd38e84585cbd3e4e279fcc136ee02fcd
Marc Ettlinger <ettlinger@gmail.com>
2016-03-19 03:24:00 -0400

    from DO

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect
+#, Quandl
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/index')
-
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-if __name__ == '__main__':
-  app.run(port=33507)
+@app.route('/graph', methods=['POST'])
+def graph():
+    symbol = request.form['ticker']
+
+    return render_template('graph.html',ticker=symbol)
+
+if __name__ == "__main__":
+    app.run(debug=True)
