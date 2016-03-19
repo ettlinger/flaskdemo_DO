3d6d4e40c88a75bac321c6cbcac1c5eb579c8be2
Marc Ettlinger <ettlinger@gmail.com>
2016-03-19 11:46:23 -0400

    fixed port:w

--- app.py
+++ app.py
@@ -23,4 +23,5 @@ def graph():
     return render_template('graph.html', script=script, div=div, ticker=symbol)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(port=33507)
+#    app.run(host='0.0.0.0', debug=True)
